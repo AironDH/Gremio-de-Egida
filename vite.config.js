1621c92e70021a80d38f67552c107618dc9b01a9
AironDH <airondh@gmail.com>
2026-07-01 23:52:46 -0400

    Vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Gremio de Égida',
+        short_name: 'Gremio de Égida',
+        description: 'Gestor de hojas de personaje DnD',
+        theme_color: '#c084fc',
+        background_color: '#e5e4e7',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'public/gremio-192.png', // No se usa '/' al inicio si usas el plugin
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'public/gremio-app-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'public/gremio-app-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable' // Iconos de Android
+          }
+        ]
+      }
+    })
+  ],
 })
